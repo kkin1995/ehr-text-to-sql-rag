426f1dea62936b0b76eedea2871e24bbae3ac239
kkin1995 <7694983+kkin1995@users.noreply.github.com>
2024-04-10 12:43:17 +0530

    Implemented a logger in the CLI alongwith some utility programs

--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,27 @@
 import argparse
 import sqlite3
 import pandas as pd
+import logging
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+from utils import DynamicPathFileHandler, get_text_hash, sanitize_filename
 from query_llm import LLMQueryHandler
+
+load_dotenv()
+LOG_DIR = os.environ.get("LOG_DIR")
+
+log_dir = LOG_DIR
+log_filename = ".log"
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = DynamicPathFileHandler(directory=log_dir, filename=log_filename)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--user_prompt")
@@ -14,6 +34,10 @@ user_prompt = args.user_prompt
 vector_store = args.vector_store
 gpt_model = args.gpt_model
 
+logger.info(user_prompt)
+logger.info(vector_store)
+logger.info(gpt_model)
+
 
 def get_sql_query(user_prompt, vector_store, gpt_model):
     handler = LLMQueryHandler(model=gpt_model, vector_store=vector_store, top_k=3)
@@ -25,8 +49,14 @@ def get_sql_query(user_prompt, vector_store, gpt_model):
 
 sql_query = get_sql_query(user_prompt, vector_store, gpt_model)
 
+logger.info(sql_query)
+
 connection = sqlite3.connect("patient_health_data.db")
-pd_execute = pd.read_sql_query(sql_query, connection)
+
+try:
+    pd_execute = pd.read_sql_query(sql_query, connection)
+except Exception as e:
+    logger.exception(f"Error Occured While Executing SQL Query: {e}")
 
 cursor = connection.cursor()
 
@@ -37,5 +67,20 @@ cols = []
 for i in range(n_cols):
     cols.append(data.description[i][0])
 
+connection.close()
+
 df = pd.DataFrame(pd_execute, columns=cols)
-print(df)
+
+output_data_dir = "/Users/karankinariwala/Library/CloudStorage/OneDrive-Personal/Medeva LLM Internship/data/output_data"
+user_prompt_sanitized = sanitize_filename(user_prompt)
+sql_query_hash = get_text_hash(sql_query)
+timestamp_str = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+output_file_name = (
+    f"query_result_{timestamp_str}_{user_prompt_sanitized}_{sql_query_hash}.csv"
+)
+output_file_path = os.path.join(output_data_dir, output_file_name)
+
+df.to_csv(output_file_path, index=False)
+
+logger.info(f"Data saved to {output_file_name}")
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,57 @@
 from dotenv import load_dotenv
 import os
+import sys
+import hashlib
+import re
+from datetime import datetime
+import logging
 
 load_dotenv()
+
+
+class DynamicPathFileHandler(logging.FileHandler):
+    def __init__(
+        self,
+        directory,
+        filename,
+        mode="a",
+        encoding=None,
+        delay=False,
+    ):
+        self.base_directory = directory
+        self.base_filename = filename
+        filepath = self._calculate_dynamic_path()
+
+        super().__init__(filepath, mode, encoding, delay)
+
+    def _calculate_dynamic_path(self):
+        date_now = datetime.now()
+        directory = os.path.join(
+            self.base_directory, str(date_now.year), date_now.strftime("%B")
+        )
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        filepath = os.path.join(
+            directory, date_now.strftime("%d%m%Y") + self.base_filename
+        )
+        self.currently_logging_to = filepath
+        return filepath
+
+    def emit(self, record):
+        module_name = os.path.basename(
+            sys.argv[0] if sys.argv[0] else "unknown_program"
+        )
+        record_program = os.path.splitext(module_name)[0]
+        super().emit(record)
+
+
+def sanitize_filename(input_str):
+    return re.sub(r"[^a-zA-Z0-9_]", "_", input_str)
+
+
+def get_text_hash(text):
+    return hashlib.md5(text.encode("utf-8")).hexdigest()[:8]
 
 
 def check_valid_vector_store(vector_store_name: str) -> bool:
