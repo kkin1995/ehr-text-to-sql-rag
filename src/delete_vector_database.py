c1f965d81f97a5171931267a84fd027a071271e5
kkin1995 <7694983+kkin1995@users.noreply.github.com>
2024-04-11 17:19:51 +0530

    Wrote documentation for function

--- src/delete_vector_database.py
+++ src/delete_vector_database.py
@@ -5,8 +5,18 @@ logger = setup_logger(__name__)
 
 
 def delete_database(
-    database_client: weaviate.Client, index_name, vector_store="weaviate"
+    database_client: weaviate.Client, index_name: str, vector_store: str = "weaviate"
 ):
+    """
+    Deletes a class from a vector database. Currently only supports weaviate.
+
+    Parameters:
+    ---
+    - weaviate_client (weaviate.Client): An instance of the Weaviate client, connected to your Weaviate instance.
+    - index_name (str): The name of the class in the Weaviate schema you wish to delete.
+    - vector_store (str, optional): The name of the vector store in which you wish to delete the index.
+    Currently only supports weaviate.
+    """
     if vector_store == "weaviate":
         try:
             database_client.schema.delete_class(index_name)
