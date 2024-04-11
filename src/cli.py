081f7fe426de56e8b1897598de4e5b59acf7429b
kkin1995 <7694983+kkin1995@users.noreply.github.com>
2024-04-11 17:16:00 +0530

    Refactored class into function for simplicity

--- src/cli.py
+++ src/cli.py
@@ -101,51 +101,22 @@ def get_column_names_from_db(db_path: str, sql_query: str) -> list[str]:
     return cols
 
 
-class DatabaseHandler:
+def execute_sql_on_db(db_path: str, query: str, params=None) -> pd.DataFrame:
     """
-    Handles database operations using a SQLite database.
+    Executes SQL query on specified SQLite3 database with parameters and returns data in a pandas DataFrame.
 
-    This class abstracts the database connection and query execution process,
-    allowing for executing SQL queries on a SQLite database and retrieving the
-    results as a pandas DataFrame.
+    Parameters:
+    ----
+    - db_path (str): The file path to the SQLite database.
+    - query (str): The SQL query to execute.
+    - params (dict, optional): Parameters to bind to the query.
 
-    Attributes:
-    ---
-    - connection (sqlite3.Connection): The connection to the SQLite database.
-
-    Methods:
-    ---
-    - execute_query(query, params=None): Executes a given SQL query and returns the result as a pandas DataFrame.
+    Returns:
+    ----
+    - pandas.DataFrame: The result of the SQL query as a DataFrame.
     """
-
-    def __init__(self, db_path: str):
-        """
-        Initializes the DatabaseHandler with a connection to the specified SQLite database.
-
-        Parameters:
-        ---
-        - db_path (str): The file path to the SQLite database.
-        """
-        self.connection = sqlite3.connect(db_path)
-
-    def execute_query(self, query: str, params=None) -> pd.DataFrame:
-        """
-        Executes a SQL query on the database and returns the result as a pandas DataFrame.
-
-        This method uses the pandas read_sql_query function to execute the query and
-        automatically convert the results into a DataFrame. It supports parameterized queries.
-
-        Parameters:
-        ----
-        - query (str): The SQL query to execute.
-        - params (dict, optional): Parameters to bind to the query, for parameterized SQL execution.
-
-        Returns:
-        ----
-        - pandas.DataFrame: The result of the SQL query as a DataFrame.
-        """
-        with self.connection:
-            return pd.read_sql_query(query, self.connection, params)
+    with sqlite3.connect(db_path) as connection:
+        return pd.read_sql_query(query, connection, params)
 
 
 sql_query = generate_sql_query(user_prompt, vector_store, gpt_model)
@@ -154,10 +125,8 @@ if sql_query is None:
     exit(1)
 logger.info(f"Generated SQL Query: {sql_query}")
 
-db_handler = DatabaseHandler(DB_PATH)
-
 try:
-    df = db_handler.execute_query(sql_query)
+    df = execute_sql_on_db(DB_PATH, sql_query)
 except Exception as e:
     logger.exception(f"Error Occured While Executing SQL Query. Exiting Program: {e}")
     exit(1)
